7b393a71c2cc7f45decc7f04621583eb28a0c729
Felipe Ferreira <fefucho08@gmail.com>
2026-05-16 15:23:20 -0300

    Created factories for generators

---
+++ src/main/java/br/unicamp/padroescriacionais/legacy/generator/RelatorioGenerator.java
@@ -0,0 +1,7 @@
+package br.unicamp.padroescriacionais.legacy.generator;
+
+import br.unicamp.padroescriacionais.legacy.domain.Relatorio;
+
+public interface RelatorioGenerator {
+    public String gerar(Relatorio relatorio);
+}
